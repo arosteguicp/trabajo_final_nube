7cca7ad62ec33f85848fe40a0d4e0ffec4efc7cd
camila <129806557+arosteguicp@users.noreply.github.com>
2023-11-21 18:47:42 -0500

    Update CrearServicio.py
    con verificacion

--- CrearServicio.py
+++ CrearServicio.py
@@ -9,8 +9,27 @@ def lambda_handler(event, context):
     var_duracion = event['duracion']
     
     dynamodb =boto3.resource('dynamodb')
-    table = dynamodb.Table('servicios')
+
+        
+    table_servicios = dynamodb.Table('servicios')
+    table_clientes = dynamodb.Table('clientes')
+    table_mascotas = dynamodb.Table('mascotas')
     
+    # Verificar si el cliente existe
+    cliente = table_clientes.get_item(Key={'cliente_id': var_cliente_id})
+    if 'Item' not in cliente:
+        return {
+            'statusCode': 400,
+            'message': 'El cliente no existe'
+        }
+    
+    # Verificar si la mascota existe
+    mascota = table_mascotas.get_item(Key={'mascota_id': var_mascota_id})
+    if 'Item' not in mascota:
+        return {
+            'statusCode': 400,
+            'message': 'La mascota no existe'
+        }
     servicio_data = {
         'cliente_id' : var_cliente_id,
         'mascota_id' : var_mascota_id,
